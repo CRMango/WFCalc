0b24cc4b1f57cf292685cb450ef53e86b8b1fa8c
CRMango <benjamin.rast@gmail.com>
2016-04-21 10:06:51 -0400

    Update ValueHolder.java

--- ValueHolder.java
+++ ValueHolder.java
@@ -1,1 +1,24 @@
-
+public class ValueHolder{
+  int hp;
+  int shields;
+  int powermax;
+  int armor;
+  int sprintspeed;
+  
+  int impactdmg;
+  int slashdmg;
+  int punctrdmg;
+  int toxindmg;
+  int heatdmg;
+  int colddmg;
+  int elecdmg;
+  int magnetdmg;
+  int blastdmg;
+  int viraldmg;
+  int gasdmg;
+  int raddmg;
+  int corrosivedmg;
+  
+  public String 
+  
+}
